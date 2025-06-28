640747be8e958761d1c4af87cc9f46fa206136b3
Andre Proenca <andreproenca@hotmail.com>
2025-06-28 15:02:52 -0400

    Access control by role, by route

--- src/main/java/com/devsuperior/bds04/entities/User.java
+++ src/main/java/com/devsuperior/bds04/entities/User.java
@@ -8,6 +8,7 @@ import java.util.Set;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
@@ -26,6 +27,9 @@ public class User implements UserDetails {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	private String name;
+	
+	@Column(unique = true)
 	private String email;
 	private String password;
 	
@@ -49,6 +53,15 @@ public class User implements UserDetails {
 
 	public void setId(Long id) {
 		this.id = id;
+	}
+	
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
 	}
 
 	public String getEmail() {
--- src/main/java/com/devsuperior/bds04/resources/EventResource.java
+++ src/main/java/com/devsuperior/bds04/resources/EventResource.java
@@ -1,9 +1,10 @@
 package com.devsuperior.bds04.resources;
 
 import java.net.URI;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,31 +14,31 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.devsuperior.bds04.dto.CityDTO;
-import com.devsuperior.bds04.services.CityService;
+import com.devsuperior.bds04.dto.EventDTO;
+import com.devsuperior.bds04.services.EventService;
 
 import jakarta.validation.Valid;
 
 @RestController
-@RequestMapping(value = "/cities")
-public class CityResource {
-
+@RequestMapping(value = "/events")
+public class EventResource {
+	
 	@Autowired
-	private CityService service;
+	private EventService service;
 	
 	@GetMapping
-	public ResponseEntity<List<CityDTO>> findAll() {
-		List<CityDTO> list = service.findAll();		
-		return ResponseEntity.ok().body(list);
+	public ResponseEntity<Page<EventDTO>> findAll(Pageable pageable) {
+		Page<EventDTO> list = service.findAllPaged(pageable);
+		return ResponseEntity.ok().body(list);		
 	}
 	
-	@PreAuthorize("hasRole('ROLE_ADMIN')")
+	@PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_CLIENT')")
 	@PostMapping
-	public ResponseEntity<CityDTO> insert(@Valid @RequestBody CityDTO dto) {
+	public ResponseEntity<EventDTO> insert(@Valid @RequestBody EventDTO dto) {
 		dto = service.insert(dto);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
 				.buildAndExpand(dto.getId()).toUri();
-		return ResponseEntity.created(uri).body(dto);
+		return ResponseEntity.created(uri).body(dto);		
 	}
-	
+
 }
